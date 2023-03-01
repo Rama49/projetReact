dd58d66b2de3ca539adb54be76f3c437556fba36
Rama Fall <rama.fall49@gmail.com>
2023-03-01 23:24:44 +0000

    projet-react

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -2,8 +2,8 @@ function Menu() {
   return (
     <div className='text-center px-5 men'>
         <p >About</p>
-        <h className='text pb-5'>We love to make great things,<span className="row just">things that matter.</span></h> 
-        <p className='p-5'>Funding handshake buyer business to business metrics ipad partnership.First mover advantage innovator succcess deployment non-disclosure.</p>
+        <h1 className='pb-5'>We love to make great things,<span className="row just">things that matter.</span></h1> 
+        <p className='fund p-5'>Funding handshake buyer business to business metrics ipad partnership.First mover advantage innovator succcess deployment non-disclosure.</p>
     </div>
       )
     }
